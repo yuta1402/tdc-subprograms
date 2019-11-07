1b1d94a141f458fbffac40e90f48e7af872e692f
gut <yuta1402souja@live.jp>
2019-11-07 21:23:01 +0900

    Add channel test (empty)

---
+++ test/channel/main.cpp
@@ -0,0 +1,9 @@
+#include "test.hpp"
+#include "tdc_test.hpp"
+
+int main()
+{
+    tdc_test();
+
+    return test::exit();
+}
---
+++ test/channel/tdc_test.hpp
@@ -0,0 +1,26 @@
+#include "test.hpp"
+#include "channel/tdc.hpp"
+
+void tdc_generate_next_drift_value_test()
+{
+}
+
+void tdc_generate_drift_sequence_test()
+{
+}
+
+void tdc_generate_signal_sequence_test()
+{
+}
+
+void tdc_generate_received_word_test()
+{
+}
+
+void tdc_test()
+{
+    tdc_generate_next_drift_value_test();
+    tdc_generate_drift_sequence_test();
+    tdc_generate_signal_sequence_test();
+    tdc_generate_received_word_test();
+}
