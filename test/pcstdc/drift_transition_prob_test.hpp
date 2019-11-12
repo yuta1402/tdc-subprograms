b41a2d137b796488430d9184ff796c7ea4450b84
yuta.ishimatsu <yuta.ishimatsu@fixstars.com>
2019-11-12 19:35:37 +0900

    Add the test of drift_transition_prob

--- test/pcstdc/drift_transition_prob_test.hpp
+++ test/pcstdc/drift_transition_prob_test.hpp
@@ -4,9 +4,34 @@
 void drift_transition_prob_test()
 {
     pcstdc::DriftTransitionProb dtp(0.0, 1.0, 2, 2);
-    std::cout << dtp(0, -2) << std::endl;
-    std::cout << dtp(0, -1) << std::endl;
-    std::cout << dtp(0,  0) << std::endl;
-    std::cout << dtp(0,  1) << std::endl;
-    std::cout << dtp(0,  2) << std::endl;
+
+    eassert(dtp(-4, -1) == 0.0000000000e+00, "dtp(-4, -1) == %lf", dtp(-4, -1));
+    eassert(dtp(-3, -1) == 9.9565174546e-02, "dtp(-3, -1) == %lf", dtp(-3, -1));
+    eassert(dtp(-2, -1) == 2.5585031548e-01, "dtp(-2, -1) == %lf", dtp(-2, -1));
+    eassert(dtp(-1, -1) == 2.8916901994e-01, "dtp(-1, -1) == %lf", dtp(-1, -1));
+    eassert(dtp( 0, -1) == 2.5585031548e-01, "dtp( 0, -1) == %lf", dtp( 0, -1));
+    eassert(dtp( 1, -1) == 9.9565174546e-02, "dtp( 1, -1) == %lf", dtp( 1, -1));
+    eassert(dtp( 2, -1) == 0.00000000000+00, "dtp( 2, -1) == %lf", dtp( 2, -1));
+    eassert(dtp( 3, -1) == 0.00000000000+00, "dtp( 3, -1) == %lf", dtp( 3, -1));
+    eassert(dtp( 4, -1) == 0.00000000000+00, "dtp( 4, -1) == %lf", dtp( 4, -1));
+
+    eassert(dtp(-4, 0) == 0.0000000000e+00, "dtp(-4, 0) == %lf", dtp(-4, 0));
+    eassert(dtp(-3, 0) == 0.0000000000e+00, "dtp(-3, 0) == %lf", dtp(-3, 0));
+    eassert(dtp(-2, 0) == 9.9565174546e-02, "dtp(-2, 0) == %lf", dtp(-2, 0));
+    eassert(dtp(-1, 0) == 2.5585031548e-01, "dtp(-1, 0) == %lf", dtp(-1, 0));
+    eassert(dtp( 0, 0) == 2.8916901994e-01, "dtp( 0, 0) == %lf", dtp( 0, 0));
+    eassert(dtp( 1, 0) == 2.5585031548e-01, "dtp( 1, 0) == %lf", dtp( 1, 0));
+    eassert(dtp( 2, 0) == 9.9565174546e-02, "dtp( 2, 0) == %lf", dtp( 2, 0));
+    eassert(dtp( 3, 0) == 0.00000000000+00, "dtp( 3, 0) == %lf", dtp( 3, 0));
+    eassert(dtp( 4, 0) == 0.00000000000+00, "dtp( 4, 0) == %lf", dtp( 4, 0));
+
+    eassert(dtp(-4, 1) == 0.0000000000e+00, "dtp(-4, 1) == %lf", dtp(-4, 1));
+    eassert(dtp(-3, 1) == 0.0000000000e+00, "dtp(-3, 1) == %lf", dtp(-3, 1));
+    eassert(dtp(-2, 1) == 0.0000000000e+00, "dtp(-2, 1) == %lf", dtp(-2, 1));
+    eassert(dtp(-1, 1) == 9.9565174546e-02, "dtp(-1, 1) == %lf", dtp(-1, 1));
+    eassert(dtp( 0, 1) == 2.5585031548e-01, "dtp( 0, 1) == %lf", dtp( 0, 1));
+    eassert(dtp( 1, 1) == 2.8916901994e-01, "dtp( 1, 1) == %lf", dtp( 1, 1));
+    eassert(dtp( 2, 1) == 2.5585031548e-01, "dtp( 2, 1) == %lf", dtp( 2, 1));
+    eassert(dtp( 3, 1) == 9.9565174546e-02, "dtp( 3, 1) == %lf", dtp( 3, 1));
+    eassert(dtp( 4, 1) == 0.00000000000+00, "dtp( 4, 1) == %lf", dtp( 4, 1));
 }
