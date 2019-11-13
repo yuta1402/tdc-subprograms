b3dc97e88fddf436c9abd157ce837d3201521419
gut <yuta1402souja@live.jp>
2019-11-13 20:07:20 +0900

    Implement the constructor of SCDecoder

--- include/pcstdc/sc_decoder.hpp
+++ include/pcstdc/sc_decoder.hpp
@@ -1,12 +1,23 @@
 #ifndef PCSTDC_SC_DECODER_HPP
 #define PCSTDC_SC_DECODER_HPP
 
+#include "channel/tdc.hpp"
+#include "pcstdc/drift_transition_prob.hpp"
+
 /*!
 @namespace pcstdc
 @brief Polar Code Subprograms for Timing Drift Channel
 */
 namespace pcstdc
 {
+    struct SCDecoderParams
+    {
+        size_t code_length;
+        size_t info_length;
+
+        int num_segments;
+    };
+
     /*!
     @class SCDecoder
     @brief Successive Cancellation Decoder
@@ -14,12 +25,15 @@ namespace pcstdc
     class SCDecoder
     {
     public:
-        SCDecoder();
+        SCDecoder(const SCDecoderParams& params, const channel::TDC& tdc);
         ~SCDecoder() = default;
 
         // TODO: Implement
 
     private:
+        SCDecoderParams params_;
+        channel::TDC tdc_;
+        DriftTransitionProb drift_transition_prob_;
     };
 }
 
--- src/pcstdc/sc_decoder.cpp
+++ src/pcstdc/sc_decoder.cpp
@@ -2,7 +2,10 @@
 
 namespace pcstdc
 {
-    SCDecoder::SCDecoder()
+    SCDecoder::SCDecoder(const SCDecoderParams& params, const channel::TDC& tdc) :
+        params_{ params },
+        tdc_{ tdc },
+        drift_transition_prob_{ tdc.params().pass_ratio, tdc.params().drift_stddev, tdc.params().max_drift, params.num_segments }
     {
     }
 }
