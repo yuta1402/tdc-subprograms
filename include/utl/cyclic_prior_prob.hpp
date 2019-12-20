726e644e2e2f5230ed54e64dfcba82aa104fe868
gut <yuta1402souja@live.jp>
2019-12-21 00:21:51 +0900

    Support two value probability

--- include/utl/cyclic_prior_prob.hpp
+++ include/utl/cyclic_prior_prob.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <array>
 
 /*!
 @namespace utl
@@ -15,29 +16,34 @@ namespace utl
     public:
         static constexpr double InitialProb = 0.5;
 
-        using iterator = std::vector<double>::iterator;
-        using const_iterator = std::vector<double>::const_iterator;
+        using value_type = std::array<double, 2>;
+        using iterator = std::vector<value_type>::iterator;
+        using const_iterator = std::vector<value_type>::const_iterator;
 
     public:
         CyclicPriorProb() = default;
 
-        CyclicPriorProb(size_t cycle, double p = InitialProb) :
+        CyclicPriorProb(size_t cycle, double p0 = InitialProb) :
+            CyclicPriorProb{ cycle, { p0, 1.0 - p0 } }
+        {}
+
+        CyclicPriorProb(size_t cycle, const value_type& p) :
             cycle_{ cycle },
             probs_( cycle, p )
         {}
 
-        CyclicPriorProb(std::initializer_list<double> list) :
+        CyclicPriorProb(std::initializer_list<value_type> list) :
             cycle_{ list.size() },
             probs_( list )
         {}
 
         ~CyclicPriorProb() = default;
 
-        double& operator()(size_t i) { return probs_[i % cycle_]; }
-        const double& operator()(size_t i) const { return probs_[i % cycle_]; }
+        value_type& operator()(size_t i) { return probs_[i % cycle_]; }
+        const value_type& operator()(size_t i) const { return probs_[i % cycle_]; }
 
-        double& operator[](size_t i) { return probs_[i % cycle_]; }
-        const double& operator[](size_t i) const { return probs_[i % cycle_]; }
+        value_type& operator[](size_t i) { return probs_[i % cycle_]; }
+        const value_type& operator[](size_t i) const { return probs_[i % cycle_]; }
 
         iterator begin() { return std::begin(probs_); }
         const_iterator begin() const { return std::begin(probs_); }
@@ -50,25 +56,30 @@ namespace utl
         static CyclicPriorProb Marker01(const size_t interval)
         {
             CyclicPriorProb prob(interval*2 + 4, 0.5);
-            prob[interval] = 1.0;
-            prob[interval + 1] = 0.0;
-            prob[interval*2 + 2] = 0.0;
-            prob[interval*2 + 3] = 1.0;
+            prob[interval][0] = 1.0;
+            prob[interval + 1][0] = 0.0;
+            prob[interval*2 + 2][0] = 0.0;
+            prob[interval*2 + 3][0] = 1.0;
+
+            prob[interval][1] = 0.0;
+            prob[interval + 1][1] = 1.0;
+            prob[interval*2 + 2][1] = 1.0;
+            prob[interval*2 + 3][1] = 0.0;
 
             return prob;
         }
 
     private:
         size_t cycle_;
-        std::vector<double> probs_;
+        std::vector<value_type> probs_;
     };
 
     inline std::ostream& operator<<(std::ostream& os, const CyclicPriorProb& p)
     {
         for (size_t i = 0; i < p.cycle(); ++i) {
-            os << p[i];
+            os << "{ " << p[i][0] << ", " << p[i][1] << " }";
             if (i < p.cycle()-1) {
-                os << ' ';
+                os << ", ";
             }
         }
 
--- test/utl/cyclic_prior_prob_test.hpp
+++ test/utl/cyclic_prior_prob_test.hpp
@@ -1,56 +1,135 @@
 #include "test.hpp"
 #include "utl/cyclic_prior_prob.hpp"
 
-void cyclic_prior_prob_test()
+void cyclic_prior_prob_constructor_test()
+{
+    {
+        utl::CyclicPriorProb p(2);
+
+        eassert(p[0][0] == 0.5, "p[0][0] == %lf", p[0][0]);
+        eassert(p[1][0] == 0.5, "p[1][0] == %lf", p[1][0]);
+        eassert(p[2][0] == 0.5, "p[2][0] == %lf", p[2][0]);
+        eassert(p[3][0] == 0.5, "p[3][0] == %lf", p[3][0]);
+
+        eassert(p[0][1] == 0.5, "p[0][1] == %lf", p[0][1]);
+        eassert(p[1][1] == 0.5, "p[1][1] == %lf", p[1][1]);
+        eassert(p[2][1] == 0.5, "p[2][1] == %lf", p[2][1]);
+        eassert(p[3][1] == 0.5, "p[3][1] == %lf", p[3][1]);
+    }
+
+    {
+        utl::CyclicPriorProb p(2, 0.6);
+
+        eassert(p[0][0] == 0.6, "p[0][0] == %lf", p[0][0]);
+        eassert(p[1][0] == 0.6, "p[1][0] == %lf", p[1][0]);
+        eassert(p[2][0] == 0.6, "p[2][0] == %lf", p[2][0]);
+        eassert(p[3][0] == 0.6, "p[3][0] == %lf", p[3][0]);
+
+        eassert(p[0][1] == 0.4, "p[0][1] == %lf", p[0][1]);
+        eassert(p[1][1] == 0.4, "p[1][1] == %lf", p[1][1]);
+        eassert(p[2][1] == 0.4, "p[2][1] == %lf", p[2][1]);
+        eassert(p[3][1] == 0.4, "p[3][1] == %lf", p[3][1]);
+    }
+}
+
+void cyclic_prior_prob_marker01_test()
 {
     {
         auto p = utl::CyclicPriorProb::Marker01(0);
-        eassert(p[0] == 1.0, "p[0] == %lf", p[0]);
-        eassert(p[1] == 0.0, "p[1] == %lf", p[1]);
-        eassert(p[2] == 0.0, "p[2] == %lf", p[2]);
-        eassert(p[3] == 1.0, "p[3] == %lf", p[3]);
+        eassert(p[0][0] == 1.0, "p[0][0] == %lf", p[0][0]);
+        eassert(p[1][0] == 0.0, "p[1][0] == %lf", p[1][0]);
+        eassert(p[2][0] == 0.0, "p[2][0] == %lf", p[2][0]);
+        eassert(p[3][0] == 1.0, "p[3][0] == %lf", p[3][0]);
 
-        eassert(p[4] == 1.0, "p[4] == %lf", p[4]);
-        eassert(p[5] == 0.0, "p[5] == %lf", p[5]);
-        eassert(p[6] == 0.0, "p[6] == %lf", p[6]);
-        eassert(p[7] == 1.0, "p[7] == %lf", p[7]);
+        eassert(p[0][1] == 0.0, "p[0][1] == %lf", p[0][1]);
+        eassert(p[1][1] == 1.0, "p[1][1] == %lf", p[1][1]);
+        eassert(p[2][1] == 1.0, "p[2][1] == %lf", p[2][1]);
+        eassert(p[3][1] == 0.0, "p[3][1] == %lf", p[3][1]);
+
+        eassert(p[4][0] == 1.0, "p[4][0] == %lf", p[4][0]);
+        eassert(p[5][0] == 0.0, "p[5][0] == %lf", p[5][0]);
+        eassert(p[6][0] == 0.0, "p[6][0] == %lf", p[6][0]);
+        eassert(p[7][0] == 1.0, "p[7][0] == %lf", p[7][0]);
+
+        eassert(p[4][1] == 0.0, "p[4][1] == %lf", p[4][1]);
+        eassert(p[5][1] == 1.0, "p[5][1] == %lf", p[5][1]);
+        eassert(p[6][1] == 1.0, "p[6][1] == %lf", p[6][1]);
+        eassert(p[7][1] == 0.0, "p[7][1] == %lf", p[7][1]);
     }
 
     {
         auto p = utl::CyclicPriorProb::Marker01(1);
-        eassert(p[0] == 0.5, "p[0] == %lf", p[0]);
-        eassert(p[1] == 1.0, "p[1] == %lf", p[1]);
-        eassert(p[2] == 0.0, "p[2] == %lf", p[2]);
-        eassert(p[3] == 0.5, "p[3] == %lf", p[3]);
-        eassert(p[4] == 0.0, "p[4] == %lf", p[4]);
-        eassert(p[5] == 1.0, "p[5] == %lf", p[5]);
+        eassert(p[0][0] == 0.5, "p[0][0] == %lf", p[0][0]);
+        eassert(p[1][0] == 1.0, "p[1][0] == %lf", p[1][0]);
+        eassert(p[2][0] == 0.0, "p[2][0] == %lf", p[2][0]);
+        eassert(p[3][0] == 0.5, "p[3][0] == %lf", p[3][0]);
+        eassert(p[4][0] == 0.0, "p[4][0] == %lf", p[4][0]);
+        eassert(p[5][0] == 1.0, "p[5][0] == %lf", p[5][0]);
 
-        eassert(p[6] == 0.5 , "p[6] == %lf" , p[6]);
-        eassert(p[7] == 1.0 , "p[7] == %lf" , p[7]);
-        eassert(p[8] == 0.0 , "p[8] == %lf" , p[8]);
-        eassert(p[9] == 0.5 , "p[9] == %lf" , p[9]);
-        eassert(p[10] == 0.0, "p[10] == %lf", p[10]);
-        eassert(p[11] == 1.0, "p[11] == %lf", p[11]);
+        eassert(p[0][1] == 0.5, "p[0][1] == %lf", p[0][1]);
+        eassert(p[1][1] == 0.0, "p[1][1] == %lf", p[1][1]);
+        eassert(p[2][1] == 1.0, "p[2][1] == %lf", p[2][1]);
+        eassert(p[3][1] == 0.5, "p[3][1] == %lf", p[3][1]);
+        eassert(p[4][1] == 1.0, "p[4][1] == %lf", p[4][1]);
+        eassert(p[5][1] == 0.0, "p[5][1] == %lf", p[5][1]);
+
+        eassert(p[6][0] == 0.5 , "p[6][0] == %lf" , p[6][0]);
+        eassert(p[7][0] == 1.0 , "p[7][0] == %lf" , p[7][0]);
+        eassert(p[8][0] == 0.0 , "p[8][0] == %lf" , p[8][0]);
+        eassert(p[9][0] == 0.5 , "p[9][0] == %lf" , p[9][0]);
+        eassert(p[10][0] == 0.0, "p[10][0] == %lf", p[10][0]);
+        eassert(p[11][0] == 1.0, "p[11][0] == %lf", p[11][0]);
+
+        eassert(p[6][1] == 0.5 , "p[6][1] == %lf" , p[6][1]);
+        eassert(p[7][1] == 0.0 , "p[7][1] == %lf" , p[7][1]);
+        eassert(p[8][1] == 1.0 , "p[8][1] == %lf" , p[8][1]);
+        eassert(p[9][1] == 0.5 , "p[9][1] == %lf" , p[9][1]);
+        eassert(p[10][1] == 1.0, "p[10][1] == %lf", p[10][1]);
+        eassert(p[11][1] == 0.0, "p[11][1] == %lf", p[11][1]);
     }
 
     {
         auto p = utl::CyclicPriorProb::Marker01(2);
-        eassert(p[0] == 0.5, "p[0] == %lf", p[0]);
-        eassert(p[1] == 0.5, "p[1] == %lf", p[1]);
-        eassert(p[2] == 1.0, "p[2] == %lf", p[2]);
-        eassert(p[3] == 0.0, "p[3] == %lf", p[3]);
-        eassert(p[4] == 0.5, "p[4] == %lf", p[4]);
-        eassert(p[5] == 0.5, "p[5] == %lf", p[5]);
-        eassert(p[6] == 0.0, "p[6] == %lf", p[6]);
-        eassert(p[7] == 1.0, "p[7] == %lf", p[7]);
+        eassert(p[0][0] == 0.5, "p[0][0] == %lf", p[0][0]);
+        eassert(p[1][0] == 0.5, "p[1][0] == %lf", p[1][0]);
+        eassert(p[2][0] == 1.0, "p[2][0] == %lf", p[2][0]);
+        eassert(p[3][0] == 0.0, "p[3][0] == %lf", p[3][0]);
+        eassert(p[4][0] == 0.5, "p[4][0] == %lf", p[4][0]);
+        eassert(p[5][0] == 0.5, "p[5][0] == %lf", p[5][0]);
+        eassert(p[6][0] == 0.0, "p[6][0] == %lf", p[6][0]);
+        eassert(p[7][0] == 1.0, "p[7][0] == %lf", p[7][0]);
 
-        eassert(p[8] == 0.5 , "p[8] == %lf" , p[8]);
-        eassert(p[9] == 0.5 , "p[9] == %lf" , p[9]);
-        eassert(p[10] == 1.0, "p[10] == %lf", p[10]);
-        eassert(p[11] == 0.0, "p[11] == %lf", p[11]);
-        eassert(p[12] == 0.5, "p[12] == %lf", p[12]);
-        eassert(p[13] == 0.5, "p[13] == %lf", p[13]);
-        eassert(p[14] == 0.0, "p[14] == %lf", p[14]);
-        eassert(p[15] == 1.0, "p[15] == %lf", p[15]);
+        eassert(p[0][1] == 0.5, "p[0][1] == %lf", p[0][1]);
+        eassert(p[1][1] == 0.5, "p[1][1] == %lf", p[1][1]);
+        eassert(p[2][1] == 0.0, "p[2][1] == %lf", p[2][1]);
+        eassert(p[3][1] == 1.0, "p[3][1] == %lf", p[3][1]);
+        eassert(p[4][1] == 0.5, "p[4][1] == %lf", p[4][1]);
+        eassert(p[5][1] == 0.5, "p[5][1] == %lf", p[5][1]);
+        eassert(p[6][1] == 1.0, "p[6][1] == %lf", p[6][1]);
+        eassert(p[7][1] == 0.0, "p[7][1] == %lf", p[7][1]);
+
+        eassert(p[8][0] == 0.5 , "p[8][0] == %lf" , p[8][0]);
+        eassert(p[9][0] == 0.5 , "p[9][0] == %lf" , p[9][0]);
+        eassert(p[10][0] == 1.0, "p[10][0] == %lf", p[10][0]);
+        eassert(p[11][0] == 0.0, "p[11][0] == %lf", p[11][0]);
+        eassert(p[12][0] == 0.5, "p[12][0] == %lf", p[12][0]);
+        eassert(p[13][0] == 0.5, "p[13][0] == %lf", p[13][0]);
+        eassert(p[14][0] == 0.0, "p[14][0] == %lf", p[14][0]);
+        eassert(p[15][0] == 1.0, "p[15][0] == %lf", p[15][0]);
+
+        eassert(p[8][1] == 0.5 , "p[8][1] == %lf" , p[8][1]);
+        eassert(p[9][1] == 0.5 , "p[9][1] == %lf" , p[9][1]);
+        eassert(p[10][1] == 0.0, "p[10][1] == %lf", p[10][1]);
+        eassert(p[11][1] == 1.0, "p[11][1] == %lf", p[11][1]);
+        eassert(p[12][1] == 0.5, "p[12][1] == %lf", p[12][1]);
+        eassert(p[13][1] == 0.5, "p[13][1] == %lf", p[13][1]);
+        eassert(p[14][1] == 1.0, "p[14][1] == %lf", p[14][1]);
+        eassert(p[15][1] == 0.0, "p[15][1] == %lf", p[15][1]);
     }
+}
+
+void cyclic_prior_prob_test()
+{
+    cyclic_prior_prob_constructor_test();
+    cyclic_prior_prob_marker01_test();
 }
