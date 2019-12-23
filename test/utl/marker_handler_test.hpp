7e3fd1b4021c0566e855890f5425a6bd8b082569
gut <yuta1402souja@live.jp>
2019-12-23 17:12:54 +0900

    Rename x_with_marker to x_marker

--- test/utl/marker_handler_test.hpp
+++ test/utl/marker_handler_test.hpp
@@ -13,81 +13,81 @@ void marker_handler_insert_marker_test()
         Eigen::RowVectorXi x(3);
         x << 0, 0, 0;
 
-        auto x_with_marker = marker_handler.insert_marker(x);
+        auto x_marker = marker_handler.insert_marker(x);
 
-        eassert(x_with_marker.size() == 5, "x_with_marker.size() == %ld", x_with_marker.size());
+        eassert(x_marker.size() == 5, "x_marker.size() == %ld", x_marker.size());
 
-        eassert(x_with_marker[0] == 0, "x_with_marker[0] == %d", x_with_marker[0]);
-        eassert(x_with_marker[1] == 0, "x_with_marker[1] == %d", x_with_marker[1]);
-        eassert(x_with_marker[2] == 0, "x_with_marker[2] == %d", x_with_marker[2]);
-        eassert(x_with_marker[3] == 1, "x_with_marker[3] == %d", x_with_marker[3]);
-        eassert(x_with_marker[4] == 0, "x_with_marker[4] == %d", x_with_marker[4]);
+        eassert(x_marker[0] == 0, "x_marker[0] == %d", x_marker[0]);
+        eassert(x_marker[1] == 0, "x_marker[1] == %d", x_marker[1]);
+        eassert(x_marker[2] == 0, "x_marker[2] == %d", x_marker[2]);
+        eassert(x_marker[3] == 1, "x_marker[3] == %d", x_marker[3]);
+        eassert(x_marker[4] == 0, "x_marker[4] == %d", x_marker[4]);
     }
 
     {
         Eigen::RowVectorXi x(3);
         x << 0, 1, 0;
 
-        auto x_with_marker = marker_handler.insert_marker(x);
+        auto x_marker = marker_handler.insert_marker(x);
 
-        eassert(x_with_marker.size() == 5, "x_with_marker.size() == %ld", x_with_marker.size());
+        eassert(x_marker.size() == 5, "x_marker.size() == %ld", x_marker.size());
 
-        eassert(x_with_marker[0] == 0, "x_with_marker[0] == %d", x_with_marker[0]);
-        eassert(x_with_marker[1] == 1, "x_with_marker[1] == %d", x_with_marker[1]);
-        eassert(x_with_marker[2] == 0, "x_with_marker[2] == %d", x_with_marker[2]);
-        eassert(x_with_marker[3] == 1, "x_with_marker[3] == %d", x_with_marker[3]);
-        eassert(x_with_marker[4] == 0, "x_with_marker[4] == %d", x_with_marker[4]);
+        eassert(x_marker[0] == 0, "x_marker[0] == %d", x_marker[0]);
+        eassert(x_marker[1] == 1, "x_marker[1] == %d", x_marker[1]);
+        eassert(x_marker[2] == 0, "x_marker[2] == %d", x_marker[2]);
+        eassert(x_marker[3] == 1, "x_marker[3] == %d", x_marker[3]);
+        eassert(x_marker[4] == 0, "x_marker[4] == %d", x_marker[4]);
     }
 
     {
         Eigen::RowVectorXi x(3);
         x << 1, 1, 1;
 
-        auto x_with_marker = marker_handler.insert_marker(x);
+        auto x_marker = marker_handler.insert_marker(x);
 
-        eassert(x_with_marker.size() == 5, "x_with_marker.size() == %ld", x_with_marker.size());
+        eassert(x_marker.size() == 5, "x_marker.size() == %ld", x_marker.size());
 
-        eassert(x_with_marker[0] == 1, "x_with_marker[0] == %d", x_with_marker[0]);
-        eassert(x_with_marker[1] == 1, "x_with_marker[1] == %d", x_with_marker[1]);
-        eassert(x_with_marker[2] == 0, "x_with_marker[2] == %d", x_with_marker[2]);
-        eassert(x_with_marker[3] == 1, "x_with_marker[3] == %d", x_with_marker[3]);
-        eassert(x_with_marker[4] == 1, "x_with_marker[4] == %d", x_with_marker[4]);
+        eassert(x_marker[0] == 1, "x_marker[0] == %d", x_marker[0]);
+        eassert(x_marker[1] == 1, "x_marker[1] == %d", x_marker[1]);
+        eassert(x_marker[2] == 0, "x_marker[2] == %d", x_marker[2]);
+        eassert(x_marker[3] == 1, "x_marker[3] == %d", x_marker[3]);
+        eassert(x_marker[4] == 1, "x_marker[4] == %d", x_marker[4]);
     }
 
     {
         Eigen::RowVectorXi x(4);
         x << 0, 0, 0, 0;
 
-        auto x_with_marker = marker_handler.insert_marker(x);
+        auto x_marker = marker_handler.insert_marker(x);
 
-        eassert(x_with_marker.size() == 8, "x_with_marker.size() == %ld", x_with_marker.size());
+        eassert(x_marker.size() == 8, "x_marker.size() == %ld", x_marker.size());
 
-        eassert(x_with_marker[0] == 0, "x_with_marker[0] == %d", x_with_marker[0]);
-        eassert(x_with_marker[1] == 0, "x_with_marker[1] == %d", x_with_marker[1]);
-        eassert(x_with_marker[2] == 0, "x_with_marker[2] == %d", x_with_marker[2]);
-        eassert(x_with_marker[3] == 1, "x_with_marker[3] == %d", x_with_marker[3]);
-        eassert(x_with_marker[4] == 0, "x_with_marker[4] == %d", x_with_marker[4]);
-        eassert(x_with_marker[5] == 0, "x_with_marker[5] == %d", x_with_marker[5]);
-        eassert(x_with_marker[6] == 1, "x_with_marker[6] == %d", x_with_marker[6]);
-        eassert(x_with_marker[7] == 0, "x_with_marker[7] == %d", x_with_marker[7]);
+        eassert(x_marker[0] == 0, "x_marker[0] == %d", x_marker[0]);
+        eassert(x_marker[1] == 0, "x_marker[1] == %d", x_marker[1]);
+        eassert(x_marker[2] == 0, "x_marker[2] == %d", x_marker[2]);
+        eassert(x_marker[3] == 1, "x_marker[3] == %d", x_marker[3]);
+        eassert(x_marker[4] == 0, "x_marker[4] == %d", x_marker[4]);
+        eassert(x_marker[5] == 0, "x_marker[5] == %d", x_marker[5]);
+        eassert(x_marker[6] == 1, "x_marker[6] == %d", x_marker[6]);
+        eassert(x_marker[7] == 0, "x_marker[7] == %d", x_marker[7]);
     }
 
     {
         Eigen::RowVectorXi x(4);
         x << 1, 1, 1, 1;
 
-        auto x_with_marker = marker_handler.insert_marker(x);
+        auto x_marker = marker_handler.insert_marker(x);
 
-        eassert(x_with_marker.size() == 8, "x_with_marker.size() == %ld", x_with_marker.size());
+        eassert(x_marker.size() == 8, "x_marker.size() == %ld", x_marker.size());
 
-        eassert(x_with_marker[0] == 1, "x_with_marker[0] == %d", x_with_marker[0]);
-        eassert(x_with_marker[1] == 1, "x_with_marker[1] == %d", x_with_marker[1]);
-        eassert(x_with_marker[2] == 0, "x_with_marker[2] == %d", x_with_marker[2]);
-        eassert(x_with_marker[3] == 1, "x_with_marker[3] == %d", x_with_marker[3]);
-        eassert(x_with_marker[4] == 1, "x_with_marker[4] == %d", x_with_marker[4]);
-        eassert(x_with_marker[5] == 1, "x_with_marker[5] == %d", x_with_marker[5]);
-        eassert(x_with_marker[6] == 1, "x_with_marker[6] == %d", x_with_marker[6]);
-        eassert(x_with_marker[7] == 0, "x_with_marker[7] == %d", x_with_marker[7]);
+        eassert(x_marker[0] == 1, "x_marker[0] == %d", x_marker[0]);
+        eassert(x_marker[1] == 1, "x_marker[1] == %d", x_marker[1]);
+        eassert(x_marker[2] == 0, "x_marker[2] == %d", x_marker[2]);
+        eassert(x_marker[3] == 1, "x_marker[3] == %d", x_marker[3]);
+        eassert(x_marker[4] == 1, "x_marker[4] == %d", x_marker[4]);
+        eassert(x_marker[5] == 1, "x_marker[5] == %d", x_marker[5]);
+        eassert(x_marker[6] == 1, "x_marker[6] == %d", x_marker[6]);
+        eassert(x_marker[7] == 0, "x_marker[7] == %d", x_marker[7]);
     }
 }
 
